45524dac28660a5e6e405174d89c911871edf459
ntrifi <trifinourdine@outlook.fr>
2024-10-14 19:06:46 +0200

    Added 3000 port to the server

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -31,11 +31,27 @@ resource "aws_security_group" "allow_http" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+terraform {
+  backend "s3" {
+    bucket         = "terraformbucket57"
+    key            = "terraform/state"
+    region         = "eu-west-3"
   }
 }
